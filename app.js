2407ce89c412907d63ae67fcd6ed7f427114c6c4
AhmadAlAraj06 <81652458+AhmadAlAraj06@users.noreply.github.com>
2021-05-11 03:01:05 +0300

    3 and 4 practis

--- app.js
+++ app.js
@@ -7,3 +7,16 @@ app.use(express.json());
 //2
 const todos = [{ todo: "wake up", isCompleted: false }, { todo: "Eat Breakfast", isCompleted: false }];
 
+//3
+app.get("/todos",(req,res)=>{
+    res.status(200);
+    res.send(todos);
+})
+
+//4
+app.post("/create/todo",(req,res)=>{
+    const newTodo={todo:req.body.todo,isCompleted:req.body.isCompleted}
+    todos.push(newTodo);
+    res.status(201);
+    res.json(newTodo)
+})
