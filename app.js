06c75033d35e42fa4e5aa7367198a7b392b6772a
AhmadAlAraj06 <81652458+AhmadAlAraj06@users.noreply.github.com>
2021-05-11 03:41:16 +0300

    5 and 6 practis

--- app.js
+++ app.js
@@ -20,3 +20,47 @@ app.post("/create/todo",(req,res)=>{
     res.status(201);
     res.json(newTodo)
 })
+
+//5
+app.put("/update/todo/:name", (req, res) => {
+    const name = req.params.name
+    let i1 = 0
+    let foundit = todo.find((elem, i) => {
+        i1 = i
+        return elem.todo === name
+    })
+    if (name) {
+        foundit = { todo: req.body.todo, isCompleted: req.body.isCompleted }
+        todo.splice(i1, 1, foundit)
+        res.status(200)
+        res.json(foundit)
+    } else {
+        res.status(404);
+        res.json("Todo not foundit");
+    }
+})
+
+
+
+//6
+app.delete("/delete/todo/:name", (req, res) => {
+    const name = req.params.name
+    let x = 0
+    const foundit = todos.find((elem, i) => {
+        x = i
+        return elem.todo === name
+    })
+    if (foundit) {
+        todos.splice(x, 1)
+        res.status(200)
+        res.json(foundit)
+    } else {
+        res.status(404);
+        res.json("Todo not foundit");
+    }
+})
+
+
+app.listen(port,()=>{
+    console.log(`Example app listening at http://localhost:${port}`);
+})
